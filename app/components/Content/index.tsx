a5eb8fe07fe2304a90093b3a487863c0a19a86e9
yeef93 <yunindafaranika@gmail.com>
2024-05-16 11:39:17 +0700

    feat:fiterbasedonnav

--- app/components/Content/index.tsx
+++ app/components/Content/index.tsx
@@ -1,11 +1,9 @@
 "use client";
 
 import React from "react";
-
-interface Product {
-  category: string;
-  name: string;
-}
+import Image from "next/image";
+import { Product } from "@/types/product";
+import notFound from "@/public/products/no-image.png";
 
 interface ContentProps {
   products: Product[];
@@ -13,6 +11,8 @@ interface ContentProps {
   productGroup: { [key: string]: Product[] };
   activeCategory: string;
 }
+
+const fallbackImageUrl = "@/public/products/no-image.png";
 
 function Content({
   products,
@@ -31,13 +31,40 @@ function Content({
     activeCategory || !productGroup
       ? null
       : categories.map((category) => (
-          <div key={category} className="flex flex-col">
-            <h1>{category}</h1>
-            <div className="grid grid-cols-2 gap-2 mt-10">
+          <div key={category} className=" p-5 flex flex-col">
+            <h1 className=" font-bold text-2xl">{category}</h1>
+            <div className="grid grid-cols-2 md:grid-cols-4 gap-2 mt-10">
               {productGroup[category].map((item, index) => (
-                <div className="border border-black p-4" key={index}>
-                  <div>Category: {item.category}</div>
-                  <div>Name: {item.name}</div>
+                <div className=" p-2 rounded-md shadow-md p-4" key={index}>
+                  <div>
+                    <Image
+                      src={item.imageUrl}
+                      alt={item.name}
+                      className="object-cover mix-blend-multiply bg-transparent"
+                      width={200}
+                      height={200}
+                    />
+                  </div>
+                  <h2 className=" font-bold text-2xl">$ {item.price}</h2>
+                  <div className="">{item.name}</div>
+                  <div className=" flex text-2xls gap-4">
+                    <button
+                      type="button"
+                      className="p-3 items-center justify-center rounded-full outline-none border-2 hover:bg-black hover:text-white"
+                    >
+                      -
+                    </button>
+                    <input
+                      className="rounded-full"
+                      placeholder="1 Kg"
+                    ></input>
+                    <button
+                      type="button"
+                      className="p-3 items-center justify-center rounded-full outline-none border-2 hover:bg-black hover:text-white"
+                    >
+                        +
+                    </button>
+                  </div>
                 </div>
               ))}
             </div>
@@ -49,10 +76,37 @@ function Content({
       {activeCategory ? (
         <div className="grid grid-cols-2 gap-2 mt-10">
           {finalProduct.map((item, index) => (
-            <div className="border border-black p-4" key={index}>
-              <div>Category: {item.category}</div>
-              <div>Name: {item.name}</div>
+            <div className=" p-2 rounded-md shadow-md p-4" key={index}>
+            <div>
+              <Image
+                src={item.imageUrl}
+                alt={item.name}
+                className="object-cover mix-blend-multiply bg-transparent"
+                width={200}
+                height={200}
+              />
             </div>
+            <h2 className=" font-bold text-2xl">$ {item.price}</h2>
+            <div className="">{item.name}</div>
+            <div className=" flex text-2xls gap-4">
+              <button
+                type="button"
+                className="p-3 items-center justify-center rounded-full outline-none border-2 hover:bg-black hover:text-white"
+              >
+                -
+              </button>
+              <input
+                className="rounded-full"
+                placeholder="1 Kg"
+              ></input>
+              <button
+                type="button"
+                className="p-3 items-center justify-center rounded-full outline-none border-2 hover:bg-black hover:text-white"
+              >
+                  +
+              </button>
+            </div>
+          </div>
           ))}
         </div>
       ) : (
